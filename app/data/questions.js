3a3a50799756879abd746da083b131e681bcc88d
Aaron Gaither <AaronGaither@coresystems.local>
2017-07-20 00:49:51 -0700

    update questions

--- app/data/questions.js
+++ app/data/questions.js
@@ -1,14 +1,14 @@
 const questions = [
-	{name: 'question 1', question: 'Always use semi-colons in Javascript.'},
-	{name: 'question 2', question: 'Code clarity is more important than terseness.'},
-	{name: 'question 3', question: 'You enjoy pair programming.'},
-	{name: 'question 4', question: 'You prefer working with a CSS framework.'},
-	{name: 'question 5', question: 'You enjoy functional programming in Javascript.'},
-	{name: 'question 6', question: 'You like to work on the "Back-End".'},
-	{name: 'question 7', question: 'Everything should be in a function.'},
-	{name: 'question 8', question: "Javascript's async nature is troublesome."},
-	{name: 'question 9', question: 'You enjoy object oriented programming in Javascript.'},
-	{name: 'question 10', question: 'Firebase is awesome.'}
+	{name: 'Question 1', question: 'Always use semi-colons in Javascript.'},
+	{name: 'Question 2', question: 'Code clarity is more important than terseness.'},
+	{name: 'Question 3', question: 'You enjoy pair programming.'},
+	{name: 'Question 4', question: 'You prefer working with a CSS framework.'},
+	{name: 'Question 5', question: 'You enjoy functional programming in Javascript.'},
+	{name: 'Question 6', question: 'Code should be self commenting.'},
+	{name: 'Question 7', question: 'Everything should be in a function.'},
+	{name: 'Question 8', question: "Javascript's async nature is troublesome."},
+	{name: 'Question 9', question: 'You enjoy object oriented programming in Javascript.'},
+	{name: 'Question 10', question: 'Firebase is awesome.'}
 ]
 
 module.exports = questions;
